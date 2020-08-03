aa34264834fd7df682fb461945fd3005be91434b
dilxat856988@gmail.com <dilxat856988@gmail.com>
2020-08-03 22:15:47 +1000

    updated code

--- src/Happy_B20.java
+++ src/Happy_B20.java
@@ -1,7 +1,13 @@
+import java.lang.reflect.Array;
+
 public class Happy_B20 {
     public static void main(String[] args) {
 
+        String mes = "B20 is always happy";
         System.out.println("B20 is always happy");
+        for(int i = 0 ;i<mes.length();i++){
+
+        }
 
     }
 }
