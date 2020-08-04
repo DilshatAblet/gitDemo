504f7d763e502750a720e5ea63f5eb4efb78199c
dilxat856988@gmail.com <dilxat856988@gmail.com>
2020-08-04 19:29:44 +1000

    dilshat

--- src/Happy_B20.java
+++ src/Happy_B20.java
@@ -2,12 +2,13 @@ import java.lang.reflect.Array;
 
 public class Happy_B20 {
     public static void main(String[] args) {
-
-        String mes = "B20 is always happy";
+        String s ="";
+        String message = "B20 is always happy";
         System.out.println("B20 is always happy");
-        for(int i = 0 ;i<mes.length();i++){
-
+        for(int i = 0 ;i<message.length();i++){
+            s+=message.charAt(i);
         }
-
+        System.out.println(s);
     }
+
 }
