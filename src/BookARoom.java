0097e0a374dc15304a9f98e0dabbe0b7f233341d
dilxat856988@gmail.com <dilxat856988@gmail.com>
2020-08-04 20:23:45 +1000

    error fixing attemp2

--- src/BookARoom.java
+++ src/BookARoom.java
@@ -1,6 +1,5 @@
 public class BookARoom {
     public static void main(String[] args) {
-<<<<<<< HEAD
 
         System.out.println("this is new one for github");
         System.out.println("this is new one for github");
@@ -10,14 +9,6 @@ public class BookARoom {
         System.out.println("this is new one for github");
         System.out.println("this is new one for github");
         System.out.println("this is new one for github");
-
-=======
-        
-        System.out.println("this is test");
-        String s = "";
->>>>>>> 4e2686a40e8301ef9ddb15b344f4aaa4e6b080b0
-
-
 
     }
 }
